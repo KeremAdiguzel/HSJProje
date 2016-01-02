d6368a85704c45af32c9910ed16ebe392e78b727
temelt <vektorel@lab3-ogr>
2016-01-02 12:27:42 +0200

    Ders Güncellemesi

--- HSJProje/src/com/vektorel/hsjp/main/HSJProje.java
+++ HSJProje/src/com/vektorel/hsjp/main/HSJProje.java
@@ -2,6 +2,17 @@ package com.vektorel.hsjp.main;
 
 public class HSJProje {
 	public static void main(String[] args) {
+		System.out.println(" Hello World ");  
 		System.out.println(" Hello World ");  //sysou Ctrl+Space
+		System.out.println(" Hello World "); 
+		System.out.println(" Hello World "); 
+		System.out.println(" Hello World "); 
+		System.out.println(" Hello World "); 
+		System.out.println(" Hello World "); 
+		System.out.println(" Hello World "); 
+		System.out.println(" Hello World "); 
+		System.out.println(" Hello World "); 
+		System.out.println(" Hello World "); 
+		
 	}
 }
