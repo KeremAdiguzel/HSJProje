66622f6c40ee341ca94c0d33a07173baab525533
atilganurdan <vektorel@LAB4-109>
2016-01-03 12:56:18 +0200

    Güncelleme

--- HSJProje/src/com/vektorel/hsjp/main/atilganurdan/HelloWorld.java
+++ HSJProje/src/com/vektorel/hsjp/main/atilganurdan/HelloWorld.java
@@ -1,8 +1,34 @@
 package com.vektorel.hsjp.main.atilganurdan;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		HelloWorld input = new HelloWorld();
+		//input.displayInput();
+		//input.getInput();
+		input.iteratingInput();
 	}
 
+	public void displayInput() {
+		System.out.println("DisplayInput starts");
+		System.out.println("Hello World");
+		System.out.println("DisplayInput finishes");
+
+	}
+	
+	public void getInput() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a String input");
+		String inputString = scan.nextLine();
+		System.out.println("The input is entered: " + inputString);
+
+	}
+	
+	public void iteratingInput() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Please enter a String input");
+		String inputString = scan.nextLine();
+		System.out.println(inputString +" "+ inputString +" " +inputString);
+	}
 }
