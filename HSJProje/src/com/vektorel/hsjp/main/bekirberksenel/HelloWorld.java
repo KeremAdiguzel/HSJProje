f580fc92d8c2877b9136328dbbd7fa98601be115
emreanil <tobb1234>
2016-01-03 12:31:04 +0200

    abc

--- HSJProje/src/com/vektorel/hsjp/main/bekirberksenel/HelloWorld.java
+++ HSJProje/src/com/vektorel/hsjp/main/bekirberksenel/HelloWorld.java
@@ -1,11 +1,44 @@
 package com.vektorel.hsjp.main.bekirberksenel;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Merhaba Dunya!"); 
+		HelloWorld test = new HelloWorld();
 	
+		test.ekranayaziyazanmetod(); 
+		
+		HelloWorld tt = new HelloWorld();
+		
+		tt.ekranayaziyazdiranmetod();
+		
 	}	
+	
+	public void ekranayaziyazanmetod() {
+		
+		System.out.println("Ekrana yazi yazan metod calismaya basladi.");
+		
+		System.out.println("Merhaba Dunya!");
+		
+		System.out.println("Ekrana yazi yazan metod bitti !");
+		
+	}
 
+	public void ekranayaziyazdiranmetod() {
+		
+		System.out.println("Ekrana yazi yazdiran metod basladi.");
+		
+		Scanner k = new Scanner(System.in);
+		
+		String KullanicininGirdigiMesaj = k.nextLine();
+		
+		System.out.println("Ekrana yazilan yazi = " + KullanicininGirdigiMesaj);
+		
+		System.out.println("Ekrana yazi yazdiran metod bitti.");
+		
+	}
 }
+
+
