6ca2adfbeddc250b5a74872c4aaa1a15b899b650
patateskafa <vektorel@LAB4-110>
2016-01-03 12:27:39 +0200

    commitim.

--- HSJProje/src/com/vektorel/hsjp/main/patateskafa/HelloWorld.java
+++ HSJProje/src/com/vektorel/hsjp/main/patateskafa/HelloWorld.java
@@ -1,9 +1,31 @@
 package com.vektorel.hsjp.main.patateskafa;
 
+import java.util.Scanner;
+
 public class HelloWorld {
+
 	public static void main(String[] args) {
+
+		HelloWorld test = new HelloWorld();
 		
+		test.ekranaMesaj();
+		
+		test.ekranaMesaj();
+		
+		test.mesajAlanMetod();
+		
+		
+	}
+
+	public void ekranaMesaj() {
+		System.out.println("ekranaMesaj çalýþmaya baþladý");
 		System.out.println("Merhaba Dünya");
-		
+		System.out.println("ekranaMesaj bitti");
+	}
+	
+	public void mesajAlanMetod() {
+	 Scanner tarayici = new Scanner(System.in);
+	 String kullaniciGirdisi = tarayici.nextLine();
+	 System.out.println("Kullanýcýnýn girdiði mesaj: " + kullaniciGirdisi);
 	}
 }
