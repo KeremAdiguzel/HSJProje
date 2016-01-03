e621943e428cc4336e75dad81569e210eb25eb09
demetbektas <demet.bektas@hotmail.com>
2016-01-03 12:25:54 +0200

    yfvjmh

--- HSJProje/src/com/vektorel/hsjp/main/alptunga/HelloWorld.java
+++ HSJProje/src/com/vektorel/hsjp/main/alptunga/HelloWorld.java
@@ -1,7 +1,26 @@
 package com.vektorel.hsjp.main.alptunga;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 	public static void main(String[] args) {
+
+		HelloWorld test = new HelloWorld();
+		test.ekranaMesajYazanMetod();
+		test.ekrandanMesajAlanMetod();
+	}
+	
+	public void ekranaMesajYazanMetod() {
 		System.out.println("Merhaba Dünya");
 	}
+	
+	
+public void ekrandanMesajAlanMetod() {
+	Scanner klavye = new Scanner(System.in);
+	String mesaj = klavye.nextLine();
+	
+	System.out.println("kullanýcýnýn girdiði mesaj =" + mesaj);
+}
+	
+
 }
